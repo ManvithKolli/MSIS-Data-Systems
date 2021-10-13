9dd33213f7cfb6062e3bc8eb4e43b65cfc614b0a
rohanbollareddi <rohanbolla@gmail.com>
2021-10-13 13:14:30 -0400

    changes

---
+++ app/public/js/index.js
@@ -0,0 +1,29 @@
+const App = {
+    data() {
+      return {
+
+        "referee":[]
+      }
+    },
+    computed: {
+        
+    },
+    methods: {
+        fetchRefereeData() {
+            fetch('/api/referees')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.referee = responseJson.results[0];
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        }
+    },
+    created() {
+        this.fetchRefereeData();
+    } 
+} 
+  
+Vue.createApp(App).mount('#App');
