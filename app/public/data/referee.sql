6b92c758d36d10a6a3c605a142ab91cac8ec26a9
tappy296 <coletappy@gmail.com>
2021-10-20 17:50:29 -0400

    SQL UPDATE

--- app/public/data/referee.sql
+++ app/public/data/referee.sql
@@ -26,12 +26,15 @@ CREATE TABLE assignment (
     referee_id varchar(10),
     game_number int(10),
     referee_assign_status int(25),
-    referee_grade varchar(4),
-    referee_skill int(10),
     PRIMARY KEY (assignment_id),
     FOREIGN KEY (referee_id) REFERENCES referee(referee_id),
     FOREIGN KEY (game_number) REFERENCES game(game_number)
 );
+
+INSERT INTO assignment (assignment_id, referee_id, game_number, referee_assign_status) VALUES 
+  (1, 2, 3, 'Assigned'),
+  (2, 3, 1, 'Assigned'),
+  (3, "" ,2,'Unassigned');
 
 INSERT INTO referee (referee_id, referee_fname, referee_age, referee_grade, referee_skill) VALUES 
   (1, 'Tom', 35, 'AA', 90),
@@ -39,7 +42,7 @@ INSERT INTO referee (referee_id, referee_fname, referee_age, referee_grade, refe
   (3,'Alan',45,'A2',95);
 
 INSERT INTO game (game_number,game_type,game_day,game_time) VALUES
-  (1, 'Club', '10/04/2021', '5:00 pm')
-  (2, 'Middle School', '10/10/2021', '7:00 pm')
-  (3, 'High School', '10/12/2021', '8:00 pm')
+  (1, 'Club', '10/04/2021', '5:00 pm'),
+  (2, 'Middle School', '10/10/2021', '7:00 pm'),
+  (3, 'High School', '10/12/2021', '8:00 pm');
 
