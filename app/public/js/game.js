8bab7f1169e7dd5366bb4b241e1dd8362294f2a9
tappy296 <coletappy@gmail.com>
2021-10-25 09:59:22 -0400

    game php created error needs addressed

--- app/public/js/game.js
+++ app/public/js/game.js
@@ -2,7 +2,9 @@ const Game = {
     data() {
       return {
 
-        "game":[]
+        "game":[],
+        gameForm: {},
+        selectedGame: null
       }
     },
     computed: {
@@ -19,6 +21,86 @@ const Game = {
             .catch( (err) => {
                 console.error(err);
             })
+        },
+        postGame(evt) {
+            console.log ("Test:", this.selectedGame);
+          if (this.selectedGame) {
+              this.postEditGame(evt);
+          } else {
+              this.postNewGame(evt);
+          }
+        },
+        postEditGame(evt) {
+          this.gameForm.game_number = this.selectedGame.game_number;
+          this.gameForm.game_number = this.selectedGame.game_number;        
+          
+          console.log("Editing!", this.gameForm);
+          fetch('api/game/update.php', {
+              method:'POST',
+              body: JSON.stringify(this.gameForm),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.game = json;
+              
+              // reset the form
+              this.handleResetEdit();
+            });
+        },
+        postNewGame(evt) {
+          fetch('api/game/create.php', {
+              method:'POST',
+              body: JSON.stringify(this.gameForm),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.offers = json;
+              
+              // reset the form
+              this.handleResetEdit();
+            });
+        },
+        postDeleteGame(g) {  
+          if ( !confirm("Are you sure you want to delete the game from " + g.game_number + "?") ) {
+              return;
+          }  
+          
+          console.log("Delete!", g);
+  
+          fetch('api/game/delete.php', {
+              method:'POST',
+              body: JSON.stringify(g),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.game = json;
+              
+              // reset the form
+              this.handleResetEdit();
+            });
+        },
+        handleEditGame(game) {
+            this.selectedGame = game;
+            this.gameForm = Object.assign({}, this.selectedGame);
+        },
+        handleResetEdit() {
+            this.selectedGame = null;
+            this.gameForm = {};
         }
     },
     created() {
